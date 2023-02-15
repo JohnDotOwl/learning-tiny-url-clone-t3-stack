c7b5aa7f6b03addacd77048e572ec667afe68de2
Jonathan T <jonathantjh@hotmail.com>
2023-02-15 09:34:07 +0800

    Implemented API

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+import { nanoid } from "nanoid";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -18,4 +19,44 @@ export const exampleRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+
+
+  shortenUrl: publicProcedure
+    .input(
+      z.object({
+        longUrl: z.string().trim().url(),
+      })
+    )
+    .mutation(async ({ ctx: { prisma }, input: { longUrl } }) => {
+      const shortUrl = nanoid(5);
+
+      const data = await prisma.url.create({
+        data: {
+          longUrl,
+          shortUrl,
+        },
+      });
+
+      return {
+        shortUrl: data.shortUrl,
+      };
+    }),
+
+    fetchLongUrl: publicProcedure
+    .input(
+      z.object({
+        shortUrl: z.string().length(5),
+      })
+    )
+    .query(async ({ ctx: { prisma }, input: { shortUrl } }) => {
+      const data = await prisma.url.findUnique({
+        where: {
+          shortUrl,
+        },
+      });
+
+      return data?.longUrl;
+    }),
+    
+
 });
